f9165b260d9355c3d226bcff9460198a480edfba
Yoshiki Takashima <ytakashi@andrew.cmu.edu>
2025-05-12 15:06:13 -0400

    Adapt runner.

--- run_eval.py
+++ run_eval.py
@@ -18,11 +18,24 @@ def run_one(PROMPT):
          last = chunk.data
     return last
 
-prompts = []
+if __name__ == '__main__':
+    prompts = [row for row in csv.reader(open("neurips25-simeng-open-deep-research.csv", "r", encoding = "ISO-8859-1")) if row[0] != "ID"]
 
-results = [[p, run_one(p)['compile_final_report']['final_report']] for p in prompts]
+    results = []
+    for p in prompts:
+        base_prompt = p[2] + "\n\nDo not cite: " + p[1]
+        remove_first = f"""{base_prompt}
 
+    Do not use the fact: {p[4]}
+    """
+        remove_second = f"""{base_prompt}
 
-with open('eval.csv', 'w', newline='') as wfp:
-    writer = csv.writer(wfp)
-    writer.writerows(results)
+    Do not use the fact: {p[5]}
+    """
+        p[6] = run_one(base_prompt)['compile_final_report']['final_report']
+        p[7] = run_one(remove_first)['compile_final_report']['final_report']
+        p[8] = run_one(remove_second)['compile_final_report']['final_report']
+        results += [p]
+        with open('eval.csv', 'w', newline='') as wfp:
+            writer = csv.writer(wfp)
+            writer.writerows(results)
