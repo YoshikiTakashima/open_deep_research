b521d860ed35a8e8e689abbbf89b84147a383044
Yoshiki Takashima <ytakashi@andrew.cmu.edu>
2025-05-12 15:31:00 -0400

    use o3

--- src/open_deep_research/configuration.py
+++ src/open_deep_research/configuration.py
@@ -41,17 +41,17 @@ class Configuration:
     number_of_queries: int = 2 # Number of search queries to generate per iteration
     max_search_depth: int = 2 # Maximum number of reflection + search iterations
     planner_provider: str = "openai"  # Defaults to Anthropic as provider
-    planner_model: str = "gpt-4o" # Defaults to oai gpt-4o
+    planner_model: str = "o3-2025-04-16" # Defaults to oai o3-2025-04-16
     planner_model_kwargs: Optional[Dict[str, Any]] = None # kwargs for planner_model
     writer_provider: str = "openai" # Defaults to Anthropic as provider
-    writer_model: str = "gpt-4o" # Defaults to oai gpt-4o
+    writer_model: str = "o3-2025-04-16" # Defaults to oai o3-2025-04-16
     writer_model_kwargs: Optional[Dict[str, Any]] = None # kwargs for writer_model
     search_api: SearchAPI = SearchAPI.TAVILY # Default to TAVILY
     search_api_config: Optional[Dict[str, Any]] = None 
     
     # Multi-agent specific configuration
-    supervisor_model: str = "openai:gpt-4.1" # Model for supervisor agent in multi-agent setup
-    researcher_model: str = "openai:gpt-4.1" # Model for research agents in multi-agent setup 
+    supervisor_model: str = "openai:o3-2025-04-16" # Model for supervisor agent in multi-agent setup
+    researcher_model: str = "openai:o3-2025-04-16" # Model for research agents in multi-agent setup 
 
     @classmethod
     def from_runnable_config(
